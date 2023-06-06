8f8cb3e2be7397bd901c38895d125b43b632b4c8
Aline Tavares <aline@crowdbotics.com>
2023-06-06 10:08:03 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,36 +1,28 @@
-import React from "react"
-import { StyleSheet, ScrollView, SafeAreaView, Text, View } from "react-native"
-import { Button } from "react-native-paper"
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView, Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 const Untitled2 = () => {
-  return (
-    <SafeAreaView style={styles.safeArea}>
-      <ScrollView
-        contentContainerStyle={{
-          backgroundColor: "rgb(255, 251, 254)",
-          padding: 10,
-          position: "relative",
-          flex: 1
-        }}
-      >
+  return <SafeAreaView style={styles.safeArea}>
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "rgb(255, 251, 254)",
+      padding: 10,
+      position: "relative",
+      flex: 1
+    }}>
         <Text style={styles.paragraph}>Testing React Native Paper</Text>
         <View>
-          <Button
-            icon="camera"
-            mode="contained"
-            onPress={() => console.log("Pressed")}
-          >
+          <Button mode="contained" onPress={() => console.log("Pressed")}>
             Press me
           </Button>
         </View>
       </ScrollView>
-    </SafeAreaView>
-  )
-}
+    </SafeAreaView>;
+};
 
 const styles = StyleSheet.create({
   safeArea: {
     height: "100%"
   }
-})
-export default Untitled2
+});
+export default Untitled2;
